9ff4b706050d923cec54fff73d1f79d1c574cbe6
aditi <aditiparihar179@gmail.com>
2025-08-03 11:39:40 +0530

    create skill section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,7 +16,7 @@ export default function Skills() {
          {/* skills categories*/}
          <div className="flex  flex-wrap justify-center gap-3 ">
            {skillsInfo.map((category)=>{
-           return ( <div key={category.title} className="space-y-4 px-1 xs:px-2 py-4 min-h-[194px] max-w-[450px] min-w-[200px] xs:min-w-[316px] lg:min-w-[406px] flex-1 bg-gray-900 rounded-2xl border border-white  shadow-[0_0_20px_1px_rgba(130,69,236,0.3)]">
+           return ( <div key={category.title} className="space-y-4 px-1 xs:px-2 py-4 min-h-[202px] max-w-[450px] min-w-[200px] xs:min-w-[316px] lg:min-w-[406px] flex-1 bg-gray-900 rounded-2xl border border-white  shadow-[0_0_20px_1px_rgba(130,69,236,0.3)]">
                  <h2 className="text-gray-400 text-2xl xs:text-3xl font-bold md:text-4xl'">{category.title}</h2>
                  <Tilt
                                   key={category.title}
@@ -29,7 +29,7 @@ export default function Skills() {
                                  <div className="grid grid-cols-2 lg:grid-cols-3 gap-1">
                    {
                      category.skills.map((skill)=>{
-                        return (<div key={skill.name} className="p-2 flex justify-center gap-1/2 xs:gap-1 items-center border-2 border-gray-700 rounded-4xl bg-transparent ">
+                        return (<div key={skill.name} className="p-2 flex justify-center gap-1 xs:gap-1 items-center border-2 border-gray-700 rounded-4xl bg-transparent ">
                             <img src={skill.logo} alt={`${skill.logo} logo`} className="w-6 h-6 xs:w-8 xs:h-8" />
                             <span className="text-gray-300 text-xs sm:text-sm font-semibold">{skill.name}</span>
                         </div>)
